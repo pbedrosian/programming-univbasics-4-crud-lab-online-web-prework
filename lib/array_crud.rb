3221328eda8624d3f115cb91a072dc9af74661b1
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-01-24 13:36:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  groomies = ["Nick", "Nathan", "Ben"]
+  groomies = ["Nick", "Nathan", "Ben", "Trevor"]
 end
 
 def add_element_to_end_of_array(array, element)
