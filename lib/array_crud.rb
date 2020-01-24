e0eef85253fe96364b1a4268820ac3830084d740
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-01-24 13:46:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ end
 
 def remove_element_from_start_of_array(array)
   groomies = ["wow", "Nick", "Nathan", "Ben"]
-  
+  wow_wow = groomies.shift
 end
 
 def retrieve_element_from_index(array, index_number)
